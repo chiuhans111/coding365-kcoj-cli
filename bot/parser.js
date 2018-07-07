e6a0b3525152f15a1b533a9dbd4d2c5039c18f32
Hans Chiu <chiuhans111@gmail.com>
2018-07-07 18:52:58 +0800

    better parser

--- bot/parser.js
+++ bot/parser.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 var RE = {
-    input: /(^\s*\n)*(-+\s*\n)?(^\s*\n)*^(((範例)|(sample))?\s*((輸入)|(input))\s*((範例)|(sample))?)[:：]?\s*-*(\n\s*$)*/gim,
-    output: /(^\s*\n)*(-+\s*\n)?(^\s*\n)*^(((範例)|(sample))?\s*((輸出)|(output))\s*((範例)|(sample))?)[:：]?\s*-*(\n\s*$)*/gim,
+    input: /(^\s*\n)*(-+\s*\n)?(^\s*\n)*^(((範例)|(sample))?\s*((輸入)|(input))\s*((範例)|(sample))?)[:：]?\s*$\n?-*(\n\s*$)*/gim,
+    output: /(^\s*\n)*(-+\s*\n)?(^\s*\n)*^(((範例)|(sample))?\s*((輸出)|(output))\s*((範例)|(sample))?)[:：]?\s*$\n?-*(\n\s*$)*/gim,
 }
 
 
@@ -15,12 +15,13 @@ module.exports = function (id, str) {
 
     str = str.replace(RE.input, '\n>>INPUT<<')
     str = str.replace(RE.output, '>>OUTPUT<<')
+    console.log(str)
     var result = {
         id,
         desc: (str.match(/([^]*?)>>INPUT<</) || { 1: '無說明' })[1]
     }
     var tests = [];
-    str.replace(/>>INPUT<<\n([^]+?)\n*>>OUTPUT<<\n([^\n]*(\n[^\n]+)*)/g, function (str, input, output) {
+    str.replace(/>>INPUT<<\n?([^]+?)\n*>>OUTPUT<<\n?([^\n]*(\n[^\n]+)*)/g, function (str, input, output) {
         tests.push({
             input: input + '\n', output: output + '\n'
         })
